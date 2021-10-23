a2cac43d716409aa3bb2c4eb32f8c254511a690f
Talha Akhter <talhaakhter01@gmail.com>
2021-10-23 17:49:06 +0500

    app updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.use('/', function(req, res, next) {
-  res.render('index');
+router.use("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.post("/api/v1/login", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    console.log("required field missing");
+    res.status(403).send("required field missing");
+    return;
+  }
+
+  console.log("req.body: ", req.body);
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      res.status(500).send("error in getting database");
+    } else {
+      if (user) {
+        if (user.password === req.body.password) {
+          res.send(user);
+        } else {
+          res.send("Authentication fail");
+        }
+      } else {
+        res.send("user not found");
+      }
+    }
+  });
+});
+
+router.post("/api/v1/signup", (req, res) => {
+  if (!req.body.email || !req.body.password || !req.body.name) {
+    console.log("required field missing");
+    res.status(403).send("required field missing");
+    return;
+  } else {
+    console.log(req.body);
+
+    let newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    newUser.save(() => {
+      console.log("data saved");
+      res.send("profile created");
+    });
+  }
+});
+
+router.delete("/api/v1/profile", (req, res) => {
+  res.send("profile deleted");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-// const cors = require("cors");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -13,6 +13,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "view/build")));
 
@@ -34,55 +35,5 @@ mongoose.connection.on("connected", () => console.log("mongoose connected"));
 mongoose.connection.on("error", (error) =>
   console.log(`mongoose error ${error.message}`)
 );
-
-app.post("/api/v1/login", (req, res) => {
-  if (!req.body.email || !req.body.password) {
-    console.log("required field missing");
-    res.status(403).send("required field missing");
-    return;
-  }
-
-  console.log("req.body: ", req.body);
-
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (err) {
-      res.status(500).send("error in getting database");
-    } else {
-      if (user) {
-        if (user.password === req.body.password) {
-          res.send(user);
-        } else {
-          res.send("Authentication fail");
-        }
-      } else {
-        res.send("user not found");
-      }
-    }
-  });
-});
-
-app.post("/api/v1/signup", (req, res) => {
-  if (!req.body.email || !req.body.password || !req.body.name) {
-    console.log("required field missing");
-    res.status(403).send("required field missing");
-    return;
-  } else {
-    console.log(req.body);
-
-    let newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    newUser.save(() => {
-      console.log("data saved");
-      res.send("profile created");
-    });
-  }
-});
-
-app.delete("/api/v1/profile", (req, res) => {
-  res.send("profile deleted");
-});
 
 module.exports = app;
